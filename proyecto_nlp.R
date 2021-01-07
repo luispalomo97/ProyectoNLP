769aaef179dfc260919f527c8b778b6e0dda1f12
luispalomo97 <luispalomo97@gmail.com>
2021-01-07 13:53:42 +0100

    Añadido el modelo de clasificacion

--- proyecto_nlp.R
+++ proyecto_nlp.R
@@ -10,6 +10,11 @@ install.packages("sentimentr")
 install.packages("tidytext")
 install.packages("textdata")
 install.packages("syuzhet")
+install.packages("naivebayes")
+install.packages("e1071")
+install.packages("caret")
+
+
 
 #Carga de paquetes
 library(ggplot2)
@@ -22,6 +27,10 @@ library(tidytext)
 library(textdata)
 library(tidyverse)
 library(syuzhet)
+library(naivebayes)
+library(e1071)
+library(caret)
+library(janitor)
 
   
 #---------------------------------------CARGA DEL DATA SET-------------------------------------------------------
@@ -45,6 +54,7 @@ ggplot(data = alexa_reviews, mapping = aes(x = valoracion, fill = as.factor(valo
 
 #Limpieza de los comentarios, solo queremos elemenos alfa-numericos
 alexa_reviews$comentario <-gsub("[^a-zA-Z0-9 ]", "", alexa_reviews$comentario)
+#alexa_reviews$comentario <-gsub(pattern = "(^ +| +$)",replacement = "",x = alexa_reviews$comentario)
 
 corpus <- Corpus(VectorSource(alexa_reviews$comentario))                                                                                                                     
 
@@ -57,13 +67,17 @@ corpus <- tm_map(corpus,removePunctuation)
 #Eliminamos los numeros que hayan en los comentarios
 corpus <- tm_map(corpus,removeNumbers)
 
-#Eliminamos los espacios en blanco entre palabas
-corpus <- tm_map(corpus,stripWhitespace)
-
 #Eliminamos aquellas palabras que no aportan información
 corpus <- tm_map(corpus,removeWords, c(stopwords("en"),"echo","alexa","music","sound","dot","set",
                                        "amazon","product","get","speaker","home","play",
                                        "device","still","time","just","will"))
+
+#Eliminamos los espacios en blanco entre palabas
+corpus <- tm_map(corpus,stripWhitespace)
+
+
+# Para crear el clasificador inicialemente nos hemos quedado con el conjunto de palabras tokenizadas anteriormente
+datafr <- data.frame("coment"= corpus$content)
 
 #-----------------------------------------CREACION MATRIZ DE TERMINOS--------------------------------------------------
 
@@ -101,20 +115,20 @@ wordcloud(words = a$word, freq = a$freq, min.freq = 1, max.words =140,
 
 
 #-----------------------------------------ANALISIS DE SENTIMIENTOS---------------------------------------------------
-afinn_vector <- get_sentiment(alexa_reviews$comentario,method = "afinn")
+afinn_vector <- get_sentiment(datafr$coment,method = "afinn")
 summary(afinn_vector)
 
 #El resultado que nos arroja esta prueba es que el analisis de sentimientos mediante affinn nos muestra un valor medio de
 # 3 lo que nos quiere decir que de media los comentarios son mas positivos, ya que la escala de affin es de -5 a 5
 
-bing_vector <- get_sentiment(alexa_reviews$comentario,method = "bing")
+bing_vector <- get_sentiment(datafr$coment,method = "bing")
 summary(bing_vector)
 
 #El resultado que nos arroja esta prueba es que el analisis de sentimientos mediante bing nos muestra un valor medio de
 # 1 lo que nos quiere decir que de media los comentarios son mas positivos, ya que la escala de bing es de -1 a 1
 
 
-syuzhet_vector <- get_sentiment(alexa_reviews$comentario,method = "syuzhet")
+syuzhet_vector <- get_sentiment(datafr$coment,method = "syuzhet")
 summary(syuzhet_vector)
 
 #El resultado que nos arroja esta prueba es que el analisis de sentimientos mediante syuzhet nos muestra un valor medio de
@@ -153,7 +167,7 @@ plot(
 #Aqui lo que se ha utilizado es la función get_nrc_sentiment, la cual devuelve una matriz de 10 columnas
 # donde a cada comentario se muesta por cada una de esas columnas apiriciones de palabras asociadas a esos
 #sentimientos
-data_sentiment<-get_nrc_sentiment(alexa_reviews$comentario)
+data_sentiment<-get_nrc_sentiment(datafr$coment)
 head (data_sentiment,10)
 data_sentiment <- colSums(data_sentiment)
 
@@ -167,4 +181,89 @@ barplot(data_sentiment,col=brewer.pal(8,"Dark2"),ylim = c(0,5000),
 barplot(data_sentiment[c("negative","positive")],ylim=c(0,5000), col = c("#BD2C2C","#2CBD52"),
         main = "Palabras de los comentarios asociados a sentimientos positivos y negativos")
 
-                    
+
+#--------------------------------Creación del clasificador bayesiano-----------------------------------------------
+
+
+
+
+#Con este clasificador bayesiano buscamos encontrar si a partir del producto, la valoración, el feedback y el numero
+# de letras por cada comentario ver si es capaz de generar un valoración similar a la que se ha hecho. Mediante
+# aquellos comentarios de caracter positivo podamos llegar a clasificar dichas valoraciones de manera similar
+#
+
+# Realizamos una conversión a etiquetas de las valoraciones, es decir, de un valor númerico a uno categorico
+firstModif_1 <- function(){
+alexamodAux <- alexa_reviews
+alexamodAux$valoracion <- as.character(alexamodAux$valoracion)
+alexamodAux[alexamodAux$valoracion== 1 | alexamodAux== 2] <- "malo"
+alexamodAux[alexamodAux== 3 | alexamodAux== 4] <- "regular"
+alexamodAux[alexamodAux== 5] <- "bueno"
+alexamodAux <- within(alexamodAux, valoracion <- factor(valoracion, labels = c("bueno","malo","regular")))
+
+#Juntamos el dataframe que contiene el conjunto de palabras del corpus y el dataframe que contiene las modificaciones
+#descritas anteriormente. Es en este punto cuando añadimos la columna NumeroLetras, que contiene el numero de palabras
+#de cada comentario
+dataset_a <- rbind(select(alexamodAux,valoracion,producto,comentario,feedback))
+dataset_a <- cbind(dataset_a,datafr)
+dataset_a <- transform(dataset_a, "NumeroLetras"=str_count(coment, '\\w+'))  
+dataset_a <- dataset_a[dataset_a$NumeroLetras > 0,]
+dataset_a$producto <-NULL
+dataset_a$comentario <-NULL
+#dataset_a$NumeroLetras <-NULL
+#dataset_a$feedback <-NULL
+
+
+return (dataset_a)
+}
+
+
+firstModif_2 <- function(){
+  alexamod <- within(alexa_reviews, valoracion <- factor(valoracion, labels = c("uno","dos","tres","cuatro","cinco")))
+  
+  #Juntamos el dataframe que contiene el conjunto de palabras del corpus y el dataframe que contiene las modificaciones
+  #descritas anteriormente. Es en este punto cuando añadimos la columna NumeroLetras, que contiene el numero de palabras
+  #de cada comentario
+  dataset_a <- rbind(select(alexamod,valoracion,producto,comentario,feedback))
+  dataset_a <- cbind(dataset_a,datafr)
+  dataset_a <- transform(dataset_a, "NumeroLetras"=str_count(coment, '\\w+'))  
+  dataset_a <- dataset_a[dataset_a$NumeroLetras > 0,]
+dataset_a$producto <-NULL
+dataset_a$comentario <-NULL
+#dataset_a$NumeroLetras <-NULL
+#dataset_a$feedback <-NULL
+  return (dataset_a)
+}
+
+
+
+#Una vez creado el dataset adecuado procedemos a crear el conjunto de test y el de train
+# El conjunto de train será del 70% y el de test 30%
+# Primero se probará los datos con las 5 labels
+primerTest <- firstModif_2()
+trainIndex=createDataPartition(primerTest$valoracion, p=0.8, list = F)
+train=primerTest[trainIndex, ]
+test=primerTest[-trainIndex, ]
+
+model <- naiveBayes(valoracion~., data = train )
+predicted<-predict(model,test)
+tab <- table(test$valoracion, predicted, dnn = c("Actual", "Predicha"))
+confusionMatrix(tab)
+
+
+
+#De forma similar a anteriormente realizamos la division de los conjuntos de train y test
+# El conjunto de train será del 70% y el de test 30%
+# Ahora los datos estarán clasificados en : malo, regular y bueno
+segundoTest <- firstModif_1()
+trainIndex=createDataPartition(segundoTest$valoracion, p=0.8, list = F)
+train=segundoTest[trainIndex, ]
+test=segundoTest[-trainIndex, ]
+
+model <- naiveBayes(valoracion~., data = train )
+predicted<-predict(model,test)
+tab <- table(test$valoracion, predicted, dnn = c("Actual", "Predicha"))
+confusionMatrix(tab)
+
+
+
